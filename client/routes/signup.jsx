940a3ede815a8f72c079119cf2f32c3e75f0393a
Max Weiner <maxweiner02@gmail.com>
2023-09-02 12:38:26 -0600

    completed signup page

--- client/routes/signup.jsx
+++ client/routes/signup.jsx
@@ -1,12 +1,119 @@
-//for '/signup' page
+// for '/register' page
 import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import CSSBaseline from '@mui/material/CssBaseline';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
+//add import MUI material UI
+
 
 //container--| username
 //         --| password
-//         --| email
-//         --| sign up button
-const signup = () => (
-    <p>Poop</p>
-  );
+//         --| login button
+//         --| signup button
+const Signup = () => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      username: data.get('username'),
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+    
+    return (
+      <Container component='main' maxWidth='xs'>
+        <CSSBaseline />
+        <Box
+          sx={{
+            marginTop: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Box sx={{
+            p: 3,
+            mb: 6,
+            borderRadius: 15,
+            color: 'white',
+            backgroundColor: 'error.light',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+
+          }}>
+            <Typography component="h1" variant='h1'>
+              CodeForge
+            </Typography>
+          </Box>
+          <Avatar sx={{ m: 2, bgcolor: 'error.light' }}>
+            <AccountCircleIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Create an Account
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            /> 
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            {/* If we want to implement the ability to 'remember' a user's username and password info <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            /> */}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="error"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Create Account
+            </Button>  
+          </Box>
+          <Link href="/" variant="body2">
+            {"Already have an account? Log in"}
+          </Link>
+        </Box>
+      </Container>
+    )
+};
   
-  export default signup;
+export default Signup;
