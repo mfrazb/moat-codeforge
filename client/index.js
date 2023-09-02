e9971a970c41a87529f56d66138bbe9040df3515
Max Weiner <maxweiner02@gmail.com>
2023-09-02 11:18:27 -0600

    completed login page, got foundation of reducers

--- client/index.js
+++ client/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './Routes/Login';
-import Main from './Routes/Main';
-import Signup from './Routes/signup';
+import { Provider } from 'react-redux';
+import store from './store';
+import Login from './routes/Login';
+import Main from './routes/Main';
+import Signup from './routes/signup';
 
 // import UserProvider from './UserProvider.js'; Maybe useful for having global state if we need it
-import './Styles/styles.css'
+import './styles/styles.css';
+
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-//   <UserProvider>
+  <Provider store={store}>
     <BrowserRouter >
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/register' element={<Signup />} />
         <Route path='/main' element={<Main />} />
       </Routes>
-    </BrowserRouter>,
-//   </UserProvider>,
-  document.getElementById('root'),
+    </BrowserRouter>
+  </Provider>
 );
