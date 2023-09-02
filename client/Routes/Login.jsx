14932f034a3c918267b72555f499190f70abbe16
Max Weiner <maxweiner02@gmail.com>
2023-09-02 09:45:42 -0600

    started writing login info

--- client/Routes/Login.jsx
+++ client/Routes/Login.jsx
@@ -2,7 +2,11 @@
 import React from 'react';
 import useEffect from 'react';
 import './styles.css'
+//add import MUI material UI
 
+export default function SendLoginInfo() {
+  const 
+}
 
 //container--| username
 //         --| password
