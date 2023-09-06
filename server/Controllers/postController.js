49d116bb73c64cd669be40a147a6f77d1e27691d
Jason Pennini <jason.pennini@gmail.com>
2023-09-06 09:54:42 -0700

    added psuedo code to getPosts method and removed duplicate .env, .gitignore package-lock.json, and package.json

--- server/Controllers/postController.js
+++ server/Controllers/postController.js
@@ -4,13 +4,18 @@ const express = require('express');
 
 const postController = {};
 
+// Takes post request properties and sets up postgres query
 postController.createPost = async (req, res, next) => {
     try {
+        // destructuring the below categories from request body
         const { title, type, category, userId, link, description } = req.body;
         const createPostQuery = `INSERT INTO posts (user_id, title, link, description, category, type) VALUES ($1, $2, $3, $4, $5, $6);`;
+        // creating paramaters to later be used in db Query
         const params = [userId, title, link, description, category, type];
+        // use query method to extract desired fields from DB
         db.query(createPostQuery, params);
         return next();
+    // if there is an error log and return string back to 
     } catch(err) {
         return next({
             log: `postController.createPost: Error ${err}`,
@@ -19,11 +24,15 @@ postController.createPost = async (req, res, next) => {
     }
 }
 
+// 
 postController.getPosts = async (req, res, next) => {
     try {
+        // deconstructs category property from request body
         const { category } = req.body;
         const getPostsQuery = `SELECT * FROM posts WHERE category = $1;`;
+        // assaign parameter as a nested array of categories 
         const params = [category];
+        console.log
         const allPosts = await db.query(getPostsQuery, params);
         res.locals.allPosts = allPosts.rows;
         return next();
--- server/Routes/UserRouter.js
+++ server/Routes/UserRouter.js
@@ -4,10 +4,12 @@ const userController = require('../Controllers/userController');
 
 const router = express.Router();
 
+// adding return as best practice
 router.post('/login', userController.verifyUser, (req, res) => {
-    res.status(200).json(res.locals.userInfo)
+    return res.status(200).json(res.locals.userInfo)
 })
 
+// adding return as best practice
 router.post('/signup', userController.uniqueInput, userController.createUser, (req, res) => {
     return res.status(200).json({message: 'User created successfully'});
 })
