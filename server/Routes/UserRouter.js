34e56ebe2f9f7f77e746aa99fd233e844b82ad54
Jason Pennini <jason.pennini@gmail.com>
2023-09-06 16:53:35 -0700

    started the session controller, cookie routes removed from router

--- server/Routes/UserRouter.js
+++ server/Routes/UserRouter.js
@@ -1,16 +1,22 @@
 const express = require('express');
 
 const userController = require('../Controllers/userController');
+const cookieController = require('../Controllers/cookieController');
+const sessionController = require('../Controllers/sessionController')
 
 const router = express.Router();
 
 // adding return as best practice
-router.post('/login', userController.verifyUser, (req, res) => {
+router.post('/login', userController.verifyUser, 
+// cookieController.setSSIDCookie
+ (req, res) => {
     return res.status(200).json(res.locals.userInfo)
 })
 
 // adding return as best practice
-router.post('/signup', userController.uniqueInput, userController.createUser, (req, res) => {
+router.post('/signup', userController.uniqueInput, userController.createUser, 
+    // cookieController.setSSIDCookie, sessionController.startSession, 
+    (req, res) => {
     return res.status(200).json({message: 'User created successfully'});
 })
 
