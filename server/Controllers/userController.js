350c0f9cb7cdd317340cc70780fe252d3fe10617
Jonathan Gray <jonathangray@Jonathans-MacBook-Air.local>
2023-09-06 18:40:37 -0400

    Fixed userController and associated tests

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,47 +1,35 @@
-const express = require('express');
 const db = require('../Models/UserModel.js');
 const bcrypt = require('bcrypt');
+require('dotenv').config();
 
-const SALT_WORK_FACTOR = 10;
+const SALT_WORK_FACTOR = Number(process.env.SALTROUNDS);
 
 const userController = {};
 
 
-// uniqueInput checks the user input to see if its valid
-userController.uniqueInput = async (req, res, next) => {
-    try {
-        const { username, email, password } = req.body
-        const areAllStringsAndDefined = [username, email, password].every(val => typeof val === 'string' && val !== undefined);
-        console.log(areAllStringsAndDefined);
-
-        if (!areAllStringsAndDefined) {
-            return res.status(400).json({ message: 'Test: Please provide all fields' });
-        }
-
-        const params = [username, email];
-        const uniqueUsernameAndEmail = `SELECT id FROM users WHERE username = $1 OR email = $2;`;
-        const result = await db.query(uniqueUsernameAndEmail, params);
-        // check result.rows.length > 0 have to redo username or email
-        if(result.rows.length > 0) {
-            return res.status(409).json({ message: 'User is already in system'});
-        }
-        return next();
-    } catch(err) {
-        return next({
-            log: `userController.uniqueInput: Error ${err}`,
-            message: { err: 'Error occurred in userController.uniqueInput'}
-        });
-    }
-}
-
-// create a new user
+/**
+ * 
+ * @param {Object} req.body 
+ * @param {String} req.body.username Username to add, must be unique, required
+ * @param {String} req.body.password Password to add, required
+ * @param {String} req.body.email Email to add, required
+ * @param {Object} res.locals
+ * @param {Number} res.locals.userId User id created from inserting into DB 
+ * @param {Function} next When invoked without an argument, moves to next middleware
+ * @returns undefined | error object
+ */
 userController.createUser = async (req, res, next) => {
     try {
         const { username, password, email } = req.body;
         const hashedPW = await bcrypt.hash(password, SALT_WORK_FACTOR);
         const params = [username, hashedPW, email];
-        const insertUserQuery = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3);`
-        db.query(insertUserQuery, params);
+        const insertUserQuery = `
+        INSERT INTO users (username, password, email) 
+        VALUES ($1, $2, $3)
+        RETURNING id;`
+        const result = await db.query(insertUserQuery, params);
+        console.log(username, result.rows);
+        res.locals.userId = await result.rows[0].id;
         return next();
     } catch(err) {
         return next({
@@ -51,11 +39,30 @@ userController.createUser = async (req, res, next) => {
     }
 }
 
+/**
+ * @todo fix error handling for no user by still running bcrypt and not letting user know
+ *       whether error was caused by username or password
+ * 
+ * This middleware checks a users username and password from a POST request. If
+ * the user is found, it stores the user_id on res.locals.userId and moves to next middleware.
+ * Otherwise it throws an error.
+ * 
+ * @param {Object} req.body 
+ * @param {String} req.body.username
+ * @param {String} req.body.password
+ * @param {Object} res.locals
+ * @param {Number} res.locals.userId User id created from inserting into DB 
+ * @param {Function} next When invoked without an argument, moves to next middleware
+ * @returns undefined | error object
+ */
 userController.verifyUser = async (req, res, next) => {
     try {
         const { username, password } = req.body;
         const params = [username];
-        const verifyUserQuery = `SELECT * FROM users WHERE username = $1;`
+        const verifyUserQuery = `
+        SELECT * 
+        FROM users 
+        WHERE username = $1;`
         const databasePW = await db.query(verifyUserQuery, params);
 
         if(databasePW.rows.length < 1) {
@@ -63,7 +70,7 @@ userController.verifyUser = async (req, res, next) => {
         }
         const match = await bcrypt.compare(password, databasePW.rows[0].password);
         // res.locals.userInfo = {user: databasePW.rows}
-        res.locals.userInfo = { username: databasePW.rows[0].username, userID: databasePW.rows[0].id }
+        res.locals.userId = databasePW.rows[0].id;
         if (match) {
             return next();
         } else {
