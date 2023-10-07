07bced46ab18770fd7041aa157845a84ef2f8c31
Max Weiner <maxweiner02@gmail.com>
2023-10-07 08:32:02 -0600

    reconfigured db connection to use config object
    Co-authored-by: Meredith Frazier Britt <mfrazb@users.noreply.github.com>

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -1,11 +1,46 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-PG_URI = process.env.PG_URI
-console.log('pgUri', PG_URI)
-const pool = new Pool({
-    connectionString: PG_URI
-}); 
+// Declare a config object to pass into new Pool()
+
+const config = {
+  max: 5,
+  idleTimeoutMillis: 30000
+};
+
+// user - 'ydpwqckz'
+// database - 'ydpwqckz' 
+// password - 'zfIxtEVDAxWvTo8tdJs63HaotaoCU1g4'
+// server -  peanut.db.elephantsql.com AKA 'host'
+
+// uri - postgres://ydpwqckz:zfIxtEVDAxWvTo8tdJs63HaotaoCU1g4@peanut.db.elephantsql.com/ydpwqckz
+
+// Add properties to config depending on NODE_ENV value
+if(process.env.NODE_ENV === 'development') {
+  config.user = 'ydpwqckz';
+  config.database = 'ydpwqckz';
+  config.password = 'zfIxtEVDAxWvTo8tdJs63HaotaoCU1g4';
+  config.host = 'peanut.db.elephantsql.com';
+  config.port = 5432;
+} else if (process.env.NODE_ENV === 'production') {
+  config.user = process.env.RDS_USERNAME;
+  config.database = process.env.RDS_DB_NAME;
+  config.password = process.env.RDS_PASSWORD;
+  config.host = process.env.RDS_HOSTNAME;
+  config.port = process.env.RDS_PORT;
+}
+
+//PG_URI = process.env.PG_URI
+//console.log('pgUri', PG_URI)
+
+// console.log('process.env is:')
+// console.log(process.env);
+
+const pool = new Pool(config); 
+
+pool.on('error', function (err, client) {
+  console.error('idle client error', err.message, err.stack);
+});
 
 module.exports = {
   query: (text, params, callback) => {
